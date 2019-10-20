3bdde5ca9053e55b9018e003e379957243930993
Reina <r.clarkemitchell@gmail.com>
2019-10-19 22:12:25 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -7,5 +7,23 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+
+  def new
+    # @student = Student.new
+  end
+
+  def create
+    # byebug
+    # @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    session[:form_params] = params.inspect
+# redirect_to new_student_path
+    redirect_to new_student_path
+  end
+
+  private
+
+   def set_student
+     @student = Student.find(params[:id])
+   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: "Joe", last_name: "Tom")
+Student.create(first_name: "Tom", last_name: "Jones")
+Student.create(first_name: "Barry", last_name: "Mannilow")
